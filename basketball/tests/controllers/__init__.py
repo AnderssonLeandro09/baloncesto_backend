63632981cbe4285b1ebef36cc2e6f919c85a55d7
Christian Leonardo Robles Maza <christian.l.robles@unl.edu.ec>
2025-12-28 18:44:36 -0500

    eliminación de importaciones inecesarias

--- basketball/tests/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Tests de controllers/API
-# fmt: off
-from basketball.tests.controllers.test_estudiante_vinculacion import *  # noqa: F401,F403,E501
-# fmt: on
--- basketball/tests/dao/__init__.py
+++
@@ -1,2 +0,0 @@
-# Tests de DAOs
-from basketball.tests.dao.test_estudiante_vinculacion import *  # noqa: F401,F403
