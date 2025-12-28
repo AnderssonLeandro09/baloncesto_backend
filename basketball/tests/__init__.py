5c1722377c0ce1659f2a9f3921192900f302fdcf
Christian Leonardo Robles Maza <christian.l.robles@unl.edu.ec>
2025-12-28 09:31:36 -0500

    Fix test discovery by simplifying tests/__init__.py

--- basketball/tests/__init__.py
+++ basketball/tests/__init__.py
@@ -1,6 +1,1 @@
 # Tests del módulo Basketball
-# Importar todos los tests de los submódulos para que Django los descubra
-
-from basketball.tests.dao import *  # noqa: F401,F403
-from basketball.tests.services import *  # noqa: F401,F403
-from basketball.tests.controllers import *  # noqa: F401,F403
