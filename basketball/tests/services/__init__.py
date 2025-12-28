2ec3fd49bc8c370825e08a08e4e63adcdcf8a7eb
Christian Leonardo Robles Maza <christian.l.robles@unl.edu.ec>
2025-12-28 17:55:57 -0500

    Comentar test

--- basketball/tests/services/__init__.py
+++ basketball/tests/services/__init__.py
@@ -1,2 +1,3 @@
 # Tests de servicios
-from basketball.tests.services.test_estudiante_vinculacion import *  # noqa: F401,F403
+#from basketball.tests.services.test_estudiante_vinculacion import *  
+# noqa: F401,F403
