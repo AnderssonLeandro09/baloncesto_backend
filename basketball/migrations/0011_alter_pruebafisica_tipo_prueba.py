20cd501462f14aed03eb130850486cb6fb2691d6
Darwin Andrés Sarango Soto <darwin.a.sarango@unl.edu.ec>
2025-12-29 18:31:59 -0500

    Cambio en el nombre de  migración para alterar el campo 'tipo_prueba' en el modelo 'pruebafísica'

--- basketball/migrations/0011_alter_pruebafisica_tipo_prueba.py
+++ basketball/migrations/0011_alter_pruebafisica_tipo_prueba.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("basketball", "0006_alter_entrenador_options"),
+        ("basketball", "0007_entrenador_eliminado"),
     ]
 
     operations = [
