66105c7c2d3f7c3057418ad0b1f1b981bc07366e
Christian Leonardo Robles Maza <christian.l.robles@unl.edu.ec>
2026-02-02 20:27:21 -0500

    Fixes URL resolution in non-Windows environments
    Ensures correct URL resolution when running in Linux environments,
    specifically when using Docker and accessing services via
    "host.docker.internal". It replaces "host.docker.internal" with
    "localhost" in the user module URL to avoid resolution issues.

--- basketball/serializers.py
+++ basketball/serializers.py
@@ -49,12 +49,15 @@ def get_user_module_token():
 
     try:
         import os
+
         user_module_url = settings.USER_MODULE_URL
-        
+
         # En Linux, host.docker.internal no se resuelve, usar localhost
         if os.name != "nt" and "host.docker.internal" in user_module_url:
-            user_module_url = user_module_url.replace("host.docker.internal", "localhost")
-        
+            user_module_url = user_module_url.replace(
+                "host.docker.internal", "localhost"
+            )
+
         admin_email = settings.USER_MODULE_ADMIN_EMAIL
         admin_password = settings.USER_MODULE_ADMIN_PASSWORD
 
